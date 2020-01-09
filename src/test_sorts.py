6b37d7f70a8b15735eecdca93024494eea58d83a
alexisveryreal <alexmaple20@gmail.com>
2020-01-09 15:37:43 -0800

    test sort initial stuff...how to send files..hm

---
+++ src/test_sorts.py
@@ -0,0 +1,16 @@
+import time 
+import copy
+import random
+
+size1 = 100
+
+
+#---Random---#
+#-------------
+# size = 100
+#---------------
+print("\nRandom Order\n-----")
+ran = [random.randint(0,span) for a in range(0, size1)]
+t1 = time.clock()
+#bubble sort here
+print("Bubble Sort(size=", str(size1), "): ", (time.clock()-t1) * 1000)
