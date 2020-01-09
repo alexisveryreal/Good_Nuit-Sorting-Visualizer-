044b85b9f178b003d6ea9dfe2828e52396aefa34
alexisveryreal <alexmaple20@gmail.com>
2020-01-09 15:18:00 -0800

    bubble sort

---
+++ src/bubble.py
@@ -0,0 +1,17 @@
+#-------------------------
+# Bubble Sort
+#-------------------------
+def bubble_sort(A):
+    for i in range (0, len(A) - 1):
+        done = True
+        for j in range (0, len(A) - i - 1):
+            if A[j] > A[j+1]:
+                A[j], A[j+1] = A[j+1], A[j]
+                done = False
+        if done:
+            return
+
+A = [5,9,1,2,4,8,6,3,7]
+print(A)
+bubble_sort(A)
+print(A)
