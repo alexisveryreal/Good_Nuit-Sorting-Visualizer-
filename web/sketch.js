0431a60a7209d3580f2940b252d5f7605641c862
alexisveryreal <alexmaple20@gmail.com>
2020-01-08 22:36:24 -0800

    Set up arrays with colors and html to work

--- web/sketch.js
+++ web/sketch.js
@@ -1,9 +1,34 @@
 //p5.js setup
 
-function setup(){
+let values = [];
+let loops = 0;
+let swaps = 0;
+let cycles = 0;
+let lines = false;
+let numLines = 200;
 
+function setup(){
+    createCanvas(windowWidth, windowHeight);
+    colorMode(HSB, height);
+    for(i = 0; i < numLines; i++){
+        values[i] = random(height);
+    }
 }
 
 function draw(){
+    background(0);
+
+    for(i = 0; i < values.length; i++){
+        let col = color(values[i], height, height);
+        let location = map(i, 0, values.length, 0, width);
+        stroke(col);
+        if(lines) {
+            line(location, height, location, height - values[i]);
+        } else {
+            if (cycles <= 10) noStroke();
+            fill(col);
+            rect(location, height-values[i], width/numLines, height);
+        }
+    }
 
 }
