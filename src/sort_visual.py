cb05503b93f9e4fbae1dd53cdbb020df016d4ff6
alexisveryreal <alexmaple20@gmail.com>
2020-01-09 16:30:09 -0800

    made bubble sort main func for sort_visual

--- src/sort_visual.py
+++ src/sort_visual.py
@@ -4,30 +4,14 @@ import sys
 eel.init('web')
 
 @eel.expose
-def merge_sort(A):
-	merge_sort2(A, 0, len(A)-1)
-
-def merge_sort2(A, first, last):
-	if first < last:
-		middle = (first + last)//2
-		merge_sort2(A, first, middle)
-		merge_sort2(A, middle+1, last)
-		merge(A, first, middle, last)
-
-def merge(A, first, middle, last):
-	L = A[first:middle+1]
-	R = A[middle+1:last+1]
-	L.append(sys.maxsize)
-	R.append(sys.maxsize)
-	i = j = 0
-
-	
-	for k in range (first, last+1):
-		if L[i] <= R[j]:
-			A[k] = L[i]
-			i += 1
-		else:
-			A[k] = R[j]
-			j += 1
+def bubble_sort(A):
+    for i in range (0, len(A) - 1):
+        done = True
+        for j in range (0, len(A) - i - 1):
+            if A[j] > A[j+1]:
+                A[j], A[j+1] = A[j+1], A[j]
+                done = False
+        if done:
+            return
 
 eel.start('index.html') 
